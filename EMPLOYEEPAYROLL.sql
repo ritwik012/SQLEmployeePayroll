3185be15da5366e5052c3562c4a47820d99b876d
“ritwik012” <rutwikgulhaneg@gmail.com>
2022-01-06 16:03:00 +0530

    [Rutwik]: Added Data INTOTable File

--- EMPLOYEEPAYROLL.sql
+++ EMPLOYEEPAYROLL.sql
@@ -7,3 +7,11 @@ use payroll_service;
 /*UC2*/
 create table employee_payroll(id int identity (1,1) primary key, Name varchar(150) NOT NULL, Salary float NOT NULL, Start date NOT NULL);
 exec sp_help employee_payroll;
+
+
+/*UC3*/
+insert into employee_payroll(Name,Salary,Start) values
+( 'Rutwik', 400000, '2021-07-15'),
+( 'John', 300000, '2021-10-13'),
+( 'Rani', 280000, '2021-05-25'),
+( 'Sai', 480000, '2021-09-10');
