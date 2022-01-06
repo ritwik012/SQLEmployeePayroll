f1320e5ea0fe7cf9f25bd2ce26d2820e7485bd62
“ritwik012” <rutwikgulhaneg@gmail.com>
2022-01-06 16:00:41 +0530

    [Rutwik]: Added Table File

--- EMPLOYEEPAYROLL.sql
+++ EMPLOYEEPAYROLL.sql
@@ -2,3 +2,8 @@
 create database payroll_service;
 
 use payroll_service;
+
+
+/*UC2*/
+create table employee_payroll(id int identity (1,1) primary key, Name varchar(150) NOT NULL, Salary float NOT NULL, Start date NOT NULL);
+exec sp_help employee_payroll;
